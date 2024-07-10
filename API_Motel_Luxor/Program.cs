ba619eac8f4edec14d05b8b1298dfc0eeb44bc4f
Denerval s Pelizario <denervalspelizario@gmail.com>
2024-07-10 12:46:37 -0300

    Refactor: adição em program.cs a "linkagem" da interface IColaboradoresRepository a classe ColaboradoresRepository

--- API_Motel_Luxor/Program.cs
+++ API_Motel_Luxor/Program.cs
@@ -1,3 +1,5 @@
+using API_Motel_Luxor.Services.Colaboradores;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Indicando que ColaboradoresRepository recebe de herança(referenciada) a interface IColaboradoresRepository
+builder.Services.AddScoped<IColaboradoresRepository, ColaboradoresRepository>();
+
 
 var app = builder.Build();
 
